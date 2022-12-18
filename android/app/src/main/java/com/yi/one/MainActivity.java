62fdc1bdea7cc13e1ebdf64c368c622eeecab9a8
Yi Jianlong <jianlong.yi@outlook.com>
2022-12-18 11:34:14 +0800

    init

---
+++ android/app/src/main/java/com/yi/one/MainActivity.java
@@ -0,0 +1,5 @@
+package com.yi.one;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yi.one',
+  appName: 'one',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
