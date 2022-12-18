ac0f055e6dfa5e4aed723f8d5c96942085f84776
Yi Jianlong <jianlong.yi@outlook.com>
2022-12-18 22:58:30 +0800

    add vscode debug config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetifyPlugin from 'vite-plugin-vuetify'
 
+import { resolve} from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.vue', '.json', '.scss'],
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
   plugins: [vue(), vuetifyPlugin({ autoImport: true })]
 })
